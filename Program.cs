afc42f2c2a06095ca49a0b4d6c6739d5785aac56
unknown <gritsunov85@gmail.com>
2022-08-19 14:32:53 +0600

    End

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Введите координаты первой точки 1 Х:");
+int number1X = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координаты первой точки 1 Y:");
+int number1Y = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координаты первой точки 1 Z:");
+int number1Z = Convert.ToInt32(Console.ReadLine());
+
+
+Console.WriteLine("Введите координаты второй точки 2 Х:");
+int number2X = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координаты второй точки 2 Y:");
+int number2Y = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координаты второй точки 2 Z:");
+int number2Z = Convert.ToInt32(Console.ReadLine());
+
+double result = Math.Sqrt(Math.Pow((number2X - number1X), 2) + Math.Pow((number2Y - number1Y), 2) + Math.Pow((number2Z - number1Z), 2));
+
+Console.WriteLine(result);
